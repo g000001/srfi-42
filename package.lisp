8bf23f1802259563c5a88b6ce5ba7401de94e548
g000001 <chiba.masaomi@gmail.com>
2011-05-04 10:35:49 +0900

    * package.lisp   export symbols

--- package.lisp
+++ package.lisp
@@ -3,7 +3,22 @@
 (cl:in-package :cl-user)
 
 (defpackage :srfi-42
-  (:export))
+  (:export :fold3-ec
+           :fold-ec
+           :list-ec
+           :append-ec
+           :string-ec
+           :string-append-ec
+           :vector-ec
+           :vector-of-length-ec
+           :sum-ec
+           :product-ec
+           :min-ec
+           :max-ec
+           :last-ec
+           :first-ec
+           :any?-ec
+           :every?-ec))
 
 (defpackage :srfi-42-internal
   (:use :srfi-42 :cl :fiveam :mbe))
