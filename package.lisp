d6f4d3c856e25a3a8653da2c49b5e2011a304c21
g000001 <chiba.masaomi@gmail.com>
2011-05-24 21:29:53 +0900

    * 真空パッケージ化

--- package.lisp
+++ package.lisp
@@ -3,9 +3,11 @@
 (cl:in-package :cl-user)
 
 (defpackage :srfi-42
+  (:use)
   (:export :fold3-ec
            :fold-ec
            :list-ec
+           :do-ec
            :append-ec
            :string-ec
            :string-append-ec
@@ -18,7 +20,8 @@
            :last-ec
            :first-ec
            :any?-ec
-           :every?-ec))
+           :every?-ec
+           :index))
 
 (defpackage :srfi-42-internal
   (:use :srfi-42 :cl :fiveam :mbe))
